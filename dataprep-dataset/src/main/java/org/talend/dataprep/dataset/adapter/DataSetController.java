dc68baa8083dfc2d19c28a20dc6315771bb65433
jbrun <jbrun@talend.com>
2018-12-17 11:54:19 +0100

    TDP-2976: remove unused import

--- dataprep-dataset/src/main/java/org/talend/dataprep/dataset/adapter/DataSetController.java
+++ dataprep-dataset/src/main/java/org/talend/dataprep/dataset/adapter/DataSetController.java
@@ -38,7 +38,6 @@ import org.apache.avro.io.EncoderFactory;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
